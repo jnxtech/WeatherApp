93c11ab2ddd3752f9da3650adb3e3e75ce701d67
jnxtech <119785977+jnxtech@users.noreply.github.com>
2023-05-17 13:43:57 +0700

    Update +page.svelte

--- +page.svelte
+++ +page.svelte
@@ -47,69 +47,76 @@
     return weatherData ? `${weatherData.current.pressure_mb} mb` : '';
   }
   
-    function getLocation() {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          try {
-            const { latitude, longitude } = position.coords;
-            const response = await fetch(
-              `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`
-            );
-            if (response.ok) {
-              weatherData = await response.json();
-              error = '';
-              city = weatherData.location.name;
-            } else {
-              weatherData = null;
-              error = 'Error retrieving weather data. Please try again.';
-            }
-          } catch (err) {
-            console.error(err);
+  function getLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        try {
+          const { latitude, longitude } = position.coords;
+          const response = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`
+          );
+          if (response.ok) {
+            weatherData = await response.json();
+            error = '';
+            city = weatherData.location.name;
+          } else {
             weatherData = null;
-            error = 'An error occurred. Please try again later.';
+            error = 'Error retrieving weather data. Please try again.';
           }
-        });
-      } else {
-        error = 'Geolocation is not supported by your browser.';
-      }
+        } catch (err) {
+          console.error(err);
+          weatherData = null;
+          error = 'An error occurred. Please try again later.';
+        }
+      });
+    } else {
+      error = 'Geolocation is not supported by your browser.';
     }
-  
-    function clearData() {
-      city = '';
-      weatherData = null;
-      error = '';
-    }
-  </script>
-  
-  <main>
-    <h1 class="heading">Weather App</h1>
-  
-    <button class="location-btn" on:click={getLocation}>Get Weather for Current Location</button>
-  
-    <hr class="divider" />
-  
-    <div class="search-container">
-      <input class="search-input" bind:value={city} placeholder="Enter a city" />
-      <button class="search-btn" on:click={getWeather}>Get Weather</button>
-    </div>
-  
-    {#if error}
-      <p class="error">{error}</p>
-    {:else if weatherData}
-      <div class="weather-container">
-        <h2 class="location">{weatherData.location.name}</h2>
-        <p class="temperature">Temperature: {weatherData.current.temp_c}°C</p>
-        <p class="condition">Condition: {weatherData.current.condition.text}</p>
-        <p class="date-time">{getCurrentDateTime()}</p>
-        <p class="details">Wind Speed: {getWindSpeed()}</p>
-        <p class="details">Humidity: {getHumidity()}</p>
-        <p class="details">Pressure: {getPressure()}</p>
-      </div>
-    {/if}
-  
-    <button class="clear-btn" on:click={clearData}>Clear</button>
+  }
 
+  function clearData() {
+    city = '';
+    weatherData = null;
+    error = '';
+  }
+</script>
+
+<main class="max-w-md mx-auto p-6 bg-gray-900 text-white h-screen flex flex-col justify-center items-center">
+  <h1 class="text-3xl font-bold mb-8 text-orange-500 animate-bounce">Weather App</h1>
+
+  <button class="location-btn mb-8 bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded" on:click={getLocation}>
+    Get Weather for Current Location
+  </button>
+
+  <div class="search-container mb-8">
+    <input class="search-input border border-white text-orange-500 placeholder-gray-400 px-4 py-2 mr-4 focus:outline-none focus:border-orange-500 transform hover:scale-105 transition-all duration-300" bind:value={city} placeholder="Enter a city" />
+    <button class="search-btn bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded transform hover:rotate-12 transition-all duration-300" on:click={getWeather}>
+      Get Weather
+    </button>
+  </div>
+
+  {#if error}
+    <p class="error text-red-500 font-semibold mb-8 animate-pulse">{error}</p>
+  {:else if weatherData}
+    <div class="weather-container bg-gray-800 p-6 rounded shadow">
+      <h2 class="location text-2xl mb-4 text-orange-500 animate-pulse">{weatherData.location.name}</h2>
+      <p class="temperature text-xl mb-2">Temperature: <span class="text-green-400">{weatherData.current.temp_c}°C</span></p>
+      <p class="condition text-lg mb-2">Condition: <span class="text-blue-400">{weatherData.current.condition.text}</span></p>
+      <p class="date-time text-sm text-gray-400 mb-4">{getCurrentDateTime()}</p>
+      <p class="details text-lg mb-2">Wind Speed: {getWindSpeed()}</p>
+      <p class="details text-lg mb-2">Humidity: {getHumidity()}</p>
+      <p class="details text-lg">Pressure: {getPressure()}</p>
+    </div>
+  {/if}
+
+  <button class="clear-btn bg-purple-500 hover:bg-purple-600 text-white font-semibold py-2 px-4 rounded mt-8 transform hover:translate-x-2 transition-all duration-300" on:click={clearData}>
+    Clear
+  </button>
 </main>
+
+
+
+
 <style>
   
   main {
